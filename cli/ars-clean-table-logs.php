12ccb91372fe406c930463aaf5d75c993c19ba34
Wilco Alsemgeest <wilco.alsemgeest@community.joomla.org>
2021-02-14 21:59:21 +0100

    CLI script to clean ARS Log table

---
+++ cli/ars-clean-table-logs.php
@@ -0,0 +1,12 @@
+<?php
+
+require dirname(__DIR__) . '/configuration.php';
+
+$config = new JConfig;
+
+$db = new mysqli($config->host, $config->user, $config->password, $config->db);
+
+if (!$db->query("TRUNACTE " . $config->dbprefix . "ars_log;")) {
+  printf("Error: %s\n", $db->error);
+}
+
