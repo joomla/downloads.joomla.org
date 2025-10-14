cb6af259a184c54a973a3df9e0c4e9ae43937402
Benjamin Trenkle <bembelimen@users.noreply.github.com>
2025-10-14 18:28:20 +0200

    Update Joomla version references in hpbanner.php

--- www/templates/joomla/html/mod_custom/hpbanner.php
+++ www/templates/joomla/html/mod_custom/hpbanner.php
@@ -17,26 +17,26 @@ JFactory::getLanguage()->load('page_home', JPATH_SITE);
 	<h2><?php echo JText::_('HP_BANNER_HEADER'); ?></h2>
 <div class="row-fluid top">
 <div class="span6">
-	<a id="latest" class="btn btn-success" title="<?php echo JText::_('HP_BANNER_DOWNLOAD'); ?>" href="<?php echo JHtml::_('content.prepare', '{arslatest item_link \'Joomla*Stable-Full_Package.zip\' Joomla! 4}'); ?>">
+	<a id="latest" class="btn btn-success" title="<?php echo JText::_('HP_BANNER_DOWNLOAD'); ?>" href="<?php echo JHtml::_('content.prepare', '{arslatest item_link \'Joomla*Stable-Full_Package.zip\' Joomla! 6}'); ?>">
 		<span class="title">
 			<i class="icon-download">  </i>
-			<?php echo JText::_('HP_BANNER_DOWNLOAD') . ' ' . JHtml::_('content.prepare', '{arslatest release Joomla! 4}'); ?>
+			<?php echo JText::_('HP_BANNER_DOWNLOAD') . ' ' . JHtml::_('content.prepare', '{arslatest release Joomla! 6}'); ?>
 		</span>
 		<br />
 		English (UK), 
-		<?php echo JHtml::_('content.prepare', '{arslatest release Joomla! 4}') . ' ' . JText::_('HP_BANNER_PACKAGE'); ?>, ZIP
+		<?php echo JHtml::_('content.prepare', '{arslatest release Joomla! 6}') . ' ' . JText::_('HP_BANNER_PACKAGE'); ?>, ZIP
 	</a>
 	<p>
-	<?php echo JText::_('HP_BANNER_DESCPART1') . ' ' . JHtml::_('content.prepare', '{arslatest release Joomla! 4}') . ' ' . JText::_('HP_BANNER_DESCPART2'); ?>
+	<?php echo JText::_('HP_BANNER_DESCPART1') . ' ' . JHtml::_('content.prepare', '{arslatest release Joomla! 6}') . ' ' . JText::_('HP_BANNER_DESCPART2'); ?>
 	<a title="<?php echo JText::_('HP_BANNER_LINKTITLE1'); ?>" href="https://www.joomla.org/core-features.html" target="_blank">
 		<?php echo JText::_('HP_BANNER_DESCPART3'); ?>
 	</a>
 	<?php echo JText::_('HP_BANNER_DESCPART4'); ?>
-	<a title="<?php echo JText::_('HP_BANNER_LINKTITLE2'); ?>" href="<?php echo JHtml::_('content.prepare', '{arslatest release_announcements Joomla! 4}'); ?>" target="_blank">
+	<a title="<?php echo JText::_('HP_BANNER_LINKTITLE2'); ?>" href="<?php echo JHtml::_('content.prepare', '{arslatest release_announcements Joomla! 6}'); ?>" target="_blank">
 	<?php echo JText::_('HP_BANNER_DESCPART5'); ?></a> 
 	<?php echo JText::_('HP_BANNER_DESCPART6'); ?></p>
 </div>
-<div class="span6"><a class="btn btn-success" title="<?php echo JText::_('HP_BANNER_DOWNLOAD'); ?> <?php echo JText::_('HP_BANNER_UPGRADEPACK'); ?>" href="<?php echo JHtml::_('content.prepare', '{arslatest release_link Joomla! 4}'); ?>">
+<div class="span6"><a class="btn btn-success" title="<?php echo JText::_('HP_BANNER_DOWNLOAD'); ?> <?php echo JText::_('HP_BANNER_UPGRADEPACK'); ?>" href="<?php echo JHtml::_('content.prepare', '{arslatest release_link Joomla! 6}'); ?>">
 <span class="title"><i class="icon-download"> </i><?php echo JText::_('HP_BANNER_UPGRADEPACK'); ?></span><br /><?php echo JText::_('HP_BANNER_UPGRADEPACKVERSION'); ?></a>
 <p><?php echo JText::_('HP_BANNER_UPDATEINSTRUCTIONS'); ?></p>
 </div>
